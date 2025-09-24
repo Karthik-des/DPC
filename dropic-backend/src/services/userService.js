93d1fe916cb192215c7403221ef4ae8d73162cc0
V karthik <karthikvenkatesh7258@gmail.com>
2025-09-24 11:02:26 +0530

    done

--- dropic-backend/src/services/userService.js
+++
@@ -1,7 +0,0 @@
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-
-exports.createUser = async (data) => {
-  const hashedPassword = await bcrypt.hash(data.password, 10);
-  return User.create({ ...data, password: hashedPassword });
-};
